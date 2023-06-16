7739ff39788a12c9eb353f3def2af0aa039b90fa
TheKaiser8 <stefano.vanzini8@gmail.com>
2023-06-16 16:08:49 +0200

    Add milestone 3 (user can filter results with inserting word or part of it)

--- src/org/lessons/java/database/Main.java
+++ src/org/lessons/java/database/Main.java
@@ -1,6 +1,7 @@
 package org.lessons.java.database;
 
 import java.sql.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,10 +10,16 @@ public class Main {
         String user = "";
         String password = "";
 
+        Scanner scan = new Scanner(System.in);
+
         // prova apertura connessione al database
         try (Connection connection = DriverManager.getConnection(url, user, password)) {
             // stampo il catalogo per verificare il nome del database a cui sono connesso
             System.out.println(connection.getCatalog());
+
+            // chiedo all'utente di inserire una parola o parte di essa per effettuare una ricerca e filtrare i risultati
+            System.out.print("Inserisci una parola o parte di essa per eseguire una ricerca: ");
+            String parameter = scan.nextLine();
 
             // preparo lo statement SQL da eseguire
             String sql = """
@@ -20,10 +27,13 @@ public class Main {
                     FROM `countries` cou
                     JOIN regions r ON r.region_id = cou.region_id
                     JOIN continents con ON con.continent_id = r.continent_id
+                    WHERE cou.name LIKE ?
                     ORDER BY cou.name;
                     """;
             // chiedo alla Connection di preparare lo statement
             try(PreparedStatement ps = connection.prepareStatement(sql)) {
+                // settiamo il parametro della query
+                ps.setString(1, "%" + parameter + "%"); // gli apici del LIKE li inserisce automaticamente il setString
                 // eseguo lo statement che restituisce un ResultSet
                 try(ResultSet rs = ps.executeQuery()) {
                     // ciclo sulle righe del ResultSet fino a quando esiste una riga succesiva
